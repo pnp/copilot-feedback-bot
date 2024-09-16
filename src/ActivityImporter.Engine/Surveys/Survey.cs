a2a17939536ddb4391b1f90ba16b1280bec1b97e
Sam Betts <sambetts@microsoft.com>
2024-09-16 11:57:06 +0200

    .

---
+++ src/ActivityImporter.Engine/Surveys/Survey.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityImporter.Engine.Surveys;
+
+internal class Survey
+{
+    public string Value { get; set; }
+}
+
+public class SurveyQuestion
+{
+    public string MyProperty { get; set; }
+}
