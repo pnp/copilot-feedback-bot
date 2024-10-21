6495906e6f0847884ad5be4410b6e42df5dfe5db
Sam Betts <sambetts@microsoft.com>
2024-10-21 13:04:37 +0200

    .

--- src/Web/web.client/src/App.tsx
+++ src/Web/web.client/src/App.tsx
@@ -2,10 +2,7 @@
 import { AuthContainer } from './AuthContainer';
 import { AppRoutes, LoginMethod } from './AppRoutes';
 import React, { useState } from 'react';
-import { TeamsFxContext } from './TeamsFxContext';
-import { msalConfig, teamsAppConfig } from './authConfig';
-import { useTeamsUserCredential } from '@microsoft/teamsfx-react';
-import { MsalProvider } from '@azure/msal-react';
+
 import { BaseAxiosApiLoader } from './api/AxiosApiLoader';
 
 export default function App() {
@@ -15,10 +12,7 @@ export default function App() {
     const [loginMethod, setLoginMethod] = useState<LoginMethod | undefined>();
 
     const [key, setKey] = React.useState(0);
-    const { theme, themeString, teamsUserCredential } = useTeamsUserCredential({
-        initiateLoginEndpoint: teamsAppConfig.startLoginPageUrl,
-        clientId: msalConfig.auth.clientId,
-    });
+
     const forceRerender = () => {
         setKey(key + 1);
         console.log("forceRerender: ", key);
@@ -28,30 +22,13 @@ export default function App() {
         setLoginMethod(method);
         console.log("Login method changed to: ", method);
     }, []);
-    React.useEffect(() => {
-        if (teamsUserCredential) {
-            setLoginMethod(LoginMethod.TeamsSSO);
-        }
-        else {
-            setLoginMethod(LoginMethod.MSAL);
-        }
-    }, [teamsUserCredential]);
-
-    React.useEffect(() => {
-
-        let loginMethodString = "Not set";
-        if (loginMethod !== undefined)
-            loginMethodString = (loginMethod === LoginMethod.MSAL ? "MSAL" : "TeamsSSO");
-        console.debug(`App: Login method is: ${loginMethodString}, TeamsFxContext: ${JSON.stringify(teamsUserCredential)}`);
-    }, [loginMethod, teamsUserCredential]);
 
     return (
         <>
-            {loginMethod === undefined ? <div>Loading...</div> :
-                <AuthContainer onApiLoaderReady={(l: BaseAxiosApiLoader) => setApiLoader(l)} loginMethod={loginMethod} loginMethodChange={loginMethodChange}>
-                    <AppRoutes apiLoader={apiLoader} onAuthReload={forceRerender} loginMethod={loginMethod} />
-                </AuthContainer>
-            }
+            <AuthContainer onApiLoaderReady={(l: BaseAxiosApiLoader) => setApiLoader(l)} loginMethodChange={loginMethodChange}>
+                <AppRoutes apiLoader={apiLoader} onAuthReload={forceRerender} loginMethod={loginMethod} />
+            </AuthContainer>
+
         </>
     );
 }
--- src/Web/web.client/src/AuthContainer.tsx
+++ src/Web/web.client/src/AuthContainer.tsx
@@ -1,26 +1,29 @@
-import React, { PropsWithChildren, useContext, useState } from 'react';
+import React, { PropsWithChildren, useState } from 'react';
 
-import { MsalProvider, useIsAuthenticated, useMsal } from "@azure/msal-react";
+import { MsalProvider, useIsAuthenticated } from "@azure/msal-react";
 import { LoginMethod } from './AppRoutes';
 import { msalConfig, teamsAppConfig } from "./authConfig";
 import { TeamsFxContext } from './TeamsFxContext';
 import { ErrorWithCode } from '@microsoft/teamsfx';
 import { PublicClientApplication } from "@azure/msal-browser";
+import { useTeamsUserCredential } from '@microsoft/teamsfx-react';
 
 import { BaseAxiosApiLoader, MsalAxiosApiLoader, TeamsSsoAxiosApiLoader } from './api/AxiosApiLoader';
 
 export const AuthContainer: React.FC<PropsWithChildren<AuthContainerProps>> = (props) => {
 
-    const msalInstance = new PublicClientApplication(msalConfig);
+    let msalInstance : PublicClientApplication | null = null;
 
     const [apiLoader, setApiLoader] = useState<BaseAxiosApiLoader | undefined>();
     const [msalInitialised, setMsalInitialised] = useState<boolean>(false);
     const isAuthenticated = useIsAuthenticated();
-    const { instance, accounts } = useMsal();
 
-    const setLoginMethod = React.useCallback((method: LoginMethod) => { props.loginMethodChange(method) }, []);
+    const [loginMethod, setLoginMethod] = useState<LoginMethod | undefined>();
 
-    const teamsUserCredential = useContext(TeamsFxContext).teamsUserCredential;
+    const { theme, themeString, teamsUserCredential } = useTeamsUserCredential({
+        initiateLoginEndpoint: teamsAppConfig.startLoginPageUrl,
+        clientId: msalConfig.auth.clientId,
+    });
 
     // Figure out if we can use Teams SSO or MSAL
     const initAuth = React.useCallback(() => {
@@ -39,17 +42,43 @@ export const AuthContainer: React.FC<PropsWithChildren<AuthContainerProps>> = (p
                 })
                 .catch((_err: ErrorWithCode) => {
                     console.warn("Teams SSO test failed. Falling back to MSAL");
-                    setLoginMethod(LoginMethod.MSAL);
                     initMsal();
                 });
         }
+        else    
+        {
+            console.debug("No Teams credentials. Using MSAL");
+            initMsal();
+        }
     }, [teamsUserCredential]);
+
+    
+    React.useEffect(() => {
+        if (teamsUserCredential) {
+            setLoginMethod(LoginMethod.TeamsSSO);
+        }
+        else {
+            setLoginMethod(LoginMethod.MSAL);
+        }
+    }, [teamsUserCredential]);
+
+    // Debug out login method
+    React.useEffect(() => {
+
+        let loginMethodString = "Not set";
+        if (loginMethod !== undefined)
+            loginMethodString = (loginMethod === LoginMethod.MSAL ? "MSAL" : "TeamsSSO");
+        console.debug(`App: Login method is: ${loginMethodString}, TeamsFxContext: ${JSON.stringify(teamsUserCredential)}`);
+    }, [loginMethod, teamsUserCredential]);
+
 
 
     const initMsal = React.useCallback(() => {
-
+        setLoginMethod(LoginMethod.MSAL);
+        if (!msalInstance) msalInstance = new PublicClientApplication(msalConfig);
+         
         if (!msalInitialised) {
-            instance.initialize().then(() => {
+            msalInstance.initialize().then(() => {
                 console.debug("MSAL initialised with client ID: " + msalConfig.auth.clientId);
                 setMsalInitialised(true);
             });
@@ -68,14 +97,14 @@ export const AuthContainer: React.FC<PropsWithChildren<AuthContainerProps>> = (p
     // Init MSAL API loader if we have an account
     React.useEffect(() => {
 
-        if (props.loginMethod !== LoginMethod.MSAL) {
+        if (loginMethod !== LoginMethod.MSAL) {
             console.debug("Not using MSAL. Skipping MSAL API loader creation");
             return;
         }
-        if (msalInitialised && isAuthenticated && !apiLoader) {
+        if (msalInitialised && isAuthenticated && msalInstance && !apiLoader) {
             console.debug("Creating MSAL API loader");
-
-            const loader = new MsalAxiosApiLoader(instance, accounts[0], teamsAppConfig.apiEndpoint);
+            const accounts = msalInstance?.getAllAccounts();
+            const loader = new MsalAxiosApiLoader(msalInstance, accounts[0], teamsAppConfig.apiEndpoint);
             setApiLoader(loader);
             props.onApiLoaderReady(loader);
         }
@@ -91,21 +120,40 @@ export const AuthContainer: React.FC<PropsWithChildren<AuthContainerProps>> = (p
 
     return (
         <>
-        {props.loginMethod
-
-        }
-            {msalInitialised &&
-                <MsalProvider instance={msalInstance}>
-                    {props.children}
-                </MsalProvider>
+            {loginMethod === undefined ?
+                <div>Checking authentication method...</div>
+                :
+                <>
+                    {loginMethod === LoginMethod.MSAL ?
+                        <>
+                            {msalInitialised && msalInstance ?
+                                <MsalProvider instance={msalInstance}>
+                                    <p>MSAL initialised</p>
+                                    {props.children}
+                                </MsalProvider>
+                                :
+                                <div>Waiting for MSAL to initialise...</div>
+                            }
+                        </>
+                        :
+                        <>
+                            {loginMethod === LoginMethod.TeamsSSO ?
+                                <TeamsFxContext.Provider value={{ theme, themeString, teamsUserCredential }}>
+                                    {props.children}
+                                </TeamsFxContext.Provider>
+                                :
+                                <div>Error: Unknown authentication type</div>
+                            }
+                        </>
+                    }
+                </>
             }
-            {props.children}</>
+        </>
     );
 }
 
 
 export interface AuthContainerProps {
     onApiLoaderReady: Function;
-    loginMethod?: LoginMethod;
     loginMethodChange: Function;
 }
